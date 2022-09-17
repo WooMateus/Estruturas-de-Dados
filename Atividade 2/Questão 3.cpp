d933a018e10fc9f8b4c03938f8520e71a2459f5b
WooMateus <89880155+WooMateus@users.noreply.github.com>
2022-09-17 08:54:09 -0300

    Create Questão 3.cpp

---
+++ Atividade 2/Questão 3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vetor[10];
+    double media = 0, soma = 0;
+    
+    cout << "Insira os valores: " << endl;
+    for (int i=0; i < 10; i++) {
+        cout << "Digite o valor do Vetor [" << i << "]: ";
+        cin >> vetor[i];
+        soma = soma + vetor[i];
+    }
+    
+    media = soma / 10;
+    cout << endl << "Média: " << media << endl;
+    
+    cout << "Valores acima da média: ";
+    for (int i = 0; i <10; i++) {
+        if (vetor[i] > media) {
+            cout << "[" << vetor[i] << "] ";
+        }
+    }
+    
+    cout << endl << "Valores abaixo da média: ";
+    for (int i = 0; i < 10; i++) {
+        if (vetor[i] < media) {
+            cout << "[" << vetor[i] << "] ";
+        }
+    }
+    
+    return 0;
+}
