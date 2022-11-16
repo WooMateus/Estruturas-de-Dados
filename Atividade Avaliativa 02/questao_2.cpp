08da38144799863deca700708e9c3309e4306e16
WooMateus <89880155+WooMateus@users.noreply.github.com>
2022-11-16 15:07:06 -0300

    Create questao_2.cpp

---
+++ Atividade Avaliativa 02/questao_2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+    /*A sequência de Fibonacci é uma sequência de números, onde o número 1 é o primeiro e
+segundo termo da ordem e os demais são originados pela soma de seus antecessores.
+1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181…
+Utilizando Função.*/
+
+int Fibonacci(int n) {
+    int pen = 1, ult = 1, ter = 0;
+    for(int i = 0; i < n; i++) {
+        cout << pen << " ";
+        ter = ult + pen;
+        pen = ult;
+        ult = ter;
+    }
+    return 0;
+}
+
+int main()
+{
+    int n = 0;
+    
+    cout << "Escreva a quantidade de números da sequência de Fibonacci: ";
+    cin >> n;
+    Fibonacci(n);
+    
+    return 0;
+}
